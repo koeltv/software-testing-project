0470fca2ade1d0dcc40824139c5f7afff7ea802d
bnemcek <150084021+bnemcek@users.noreply.github.com>
2023-11-16 21:44:30 +0000

    modified:   test/add.test.js 	new file:   test/at.test.js

--- test/add.test.js
+++ test/add.test.js
@@ -7,4 +7,13 @@ describe("Sum", () => {
     it("adds positive number to positive number", () => {
         expect(add(3, 6)).to.equal(9)
     });
+    it("adds negative number to positive number", () => {
+        expect(add(-3, 6)).to.equal(3)
+    });
+    it("adds positive number to negative number", () => {
+        expect(add(6, -3)).to.equal(3)
+    });
+    it("adds negative number to negative number", () => {
+        expect(add(-6, -3)).to.equal(-9)
+    });
 })
---
+++ test/at.test.js
@@ -0,0 +1,17 @@
+import chai from "chai"
+import at from "../src/at.js";
+
+const expect = chai.expect
+
+describe('at ', () => {
+    it('return correct value for specific path ', () => {
+        const tmp_object = { a: [{ b: { c: 3 } }, 4] };
+        const paths = ['a[0].b.c', 'a[1]'];
+        const expected_values = [3, 4];
+
+        const tmp_values = at(tmp_object, ...paths);
+    
+        expect(tmp_values).to.eql(expected_values);
+    });
+
+});
