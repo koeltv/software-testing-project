7a4cfc4573c6836d5c54aef3fef48999bdf73a26
bnemcek <150084021+bnemcek@users.noreply.github.com>
2023-11-20 09:26:28 +0000

    modified:   test/add.test.js 	modified:   test/at.test.js 	modified:   test/divide.test.js

--- test/add.test.js
+++ test/add.test.js
@@ -4,16 +4,27 @@ import add from "../src/add.js";
 const expect = chai.expect
 
 describe("Sum", () => {
-    it("adds positive number to positive number", () => {
+    it("adds positive integer number to positive integer number (adding prices prices)", () => {
         expect(add(3, 6)).to.equal(9)
     });
-    it("adds negative number to positive number", () => {
-        expect(add(-3, 6)).to.equal(3)
+    it("adds positive float number to positive integer number (adding prices prices)", () => {
+        expect(add(3.56, 6)).to.equal(9.56)
     });
-    it("adds positive number to negative number", () => {
+    it("adds positive float number to positive float number (adding prices prices)", () => {
+        expect(add(3.56, 6.1)).to.equal(9.66)
+    });
+    it("adds positive integer number to negative integer number (degrease prices)", () => {
         expect(add(6, -3)).to.equal(3)
     });
-    it("adds negative number to negative number", () => {
-        expect(add(-6, -3)).to.equal(-9)
+    it("adds positive integer number to negative float number (degrease prices)", () => {
+        expect(add(6, -3.56)).to.equal(2.44)
     });
+
+    it("adds positive float number to negative integer number (degrease prices)", () => {
+        expect(add(6.56, -3)).to.equal(3.56)
+    });
+    it("adds positive float number to negative float number (degrease prices)", () => {
+        expect(add(6.56, -3.1)).to.equal(3.46)
+    });
+   
 })
--- test/divide.test.js
+++ test/divide.test.js
@@ -5,8 +5,9 @@ const expect = chai.expect
 
 describe('divide ', () => {
     it("devide two positive integer numbers ", () => {
-            expect(divide(6, 3)).to.equal(2)
+            expect(divide(6, 2)).to.equal(3)
     });
+    /**  May be cuppons are not the best usage for division :D so we have to find how it should be usable in cupons, may be half price....
     it("devide two positive float numbers ", () => {
             expect(divide(4.5, 1.5)).to.equal(3)
     });
@@ -19,6 +20,6 @@ describe('divide ', () => {
     it("devide by zero ", () => {
         expect(divide(6, 0)).to.throw('Cannot divide by zero')
     });
-
+*/
 
 });
